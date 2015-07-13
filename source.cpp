39ee144817fa1a56c3ecd3a1e373a87e38e9304b
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-13 17:07:52 -0500

    fibonacci recursion example

--- source.cpp
+++ source.cpp
@@ -9,9 +9,16 @@
 
 using namespace std;
 
+int fib(int n) {
+	if (n == 0 || n == 1)
+		return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main () {
 
-	
+	int n = 4;
+	cout << fib(n);
 
 	return 0;
 }
